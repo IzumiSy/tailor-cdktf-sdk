86cf66d25dbd6c78834c0217db4d35ca3e4502d6
IzumiSy <beetle-noise@gmx.com>
2024-11-06 20:20:21 +0900

    Expose defineTable and permitByIDs

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,3 +1,4 @@
 export { createBarebone } from "./barebone";
 export { addOIDCAuth } from "./auth/idp";
 export { userTable } from "./auth/table";
+export { defineTable, permitByIDs } from "./tailordb";
